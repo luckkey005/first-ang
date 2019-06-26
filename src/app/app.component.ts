683f4d1a7df91ff7273f6dc7e0fcd466c211172d
lakshmi deepak <deepak@techstargroup.com>
2019-06-26 17:18:45 +0530

    color changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular'; 
+  navbarCollapsed = true;
+
+toggleNavbarCollapsing() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+}
 }
  
